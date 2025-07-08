f3e1496a143ca82b0379a96531909cea485e2b09
venkataeswarachi <145089724+venkataeswarachi@users.noreply.github.com>
2025-07-08 21:07:07 +0530

    Update Number_of_editors_by_user_edit_bucket.sql

--- Number_of_editors_by_user_edit_bucket.sql
+++ Number_of_editors_by_user_edit_bucket.sql
@@ -1,24 +1,18 @@
--- Counting the number of actors in different contribution buckets
+-- Count number of actors in different contribution buckets
 WITH revision_buckets AS (
-    SELECT COUNT(*), bucket 
-    FROM (
-        -- Subquery: Categorizing actors based on the number of revisions they made
-        SELECT rev_actor,
+    SELECT 
         CASE 
-            WHEN COUNT(rev_id) = 0 THEN '0'               -- No revisions
-            WHEN COUNT(rev_id) BETWEEN 1 AND 5 THEN '1-5' -- Between 1 and 5 revisions
-            WHEN COUNT(rev_id) BETWEEN 6 AND 99 THEN '6-99' -- Between 6 and 99 revisions
-            WHEN COUNT(rev_id) BETWEEN 100 AND 999 THEN '100-999' -- Between 100 and 999 revisions
-            WHEN COUNT(rev_id) BETWEEN 1000 AND 4999 THEN '1000-4999' -- Between 1000 and 4999 revisions
-            ELSE '5000+' -- More than 5000 revisions
+            WHEN COUNT(rev_id) = 0 THEN '0'
+            WHEN COUNT(rev_id) BETWEEN 1 AND 5 THEN '1-5'
+            WHEN COUNT(rev_id) BETWEEN 6 AND 99 THEN '6-99'
+            WHEN COUNT(rev_id) BETWEEN 100 AND 999 THEN '100-999'
+            WHEN COUNT(rev_id) BETWEEN 1000 AND 4999 THEN '1000-4999'
+            ELSE '5000+'
         END AS bucket
-        FROM revision
-        GROUP BY rev_actor -- Grouping by actor to count their revisions
-    )
-
-SELECT
-    bucket,
-    COUNT(*) AS number_of_editors   -- Counting the number of actors in each bucket
+    FROM revision
+    GROUP BY rev_actor
+) 
+SELECT bucket, COUNT(*) AS number_of_editors
 FROM revision_buckets
 GROUP BY bucket
 ORDER BY bucket;
